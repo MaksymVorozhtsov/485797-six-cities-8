a9b58a0923c0c434a4edcc54d73427b39a5a1618
Max Vorozhtsov <maxim.vorojtsov@gmail.com>
2021-10-12 20:53:35 +0200

    error + routes

---
+++ project/src/components/error/error.tsx
@@ -0,0 +1,53 @@
+import Header from '../UI/header';
+import Tabs from '../UI/tabs';
+import Card from '../card/card';
+
+function Main(props: {numberOfPlaces: string}): JSX.Element {
+  return (
+    <div className="page page--gray page--main">
+
+      <Header />
+
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+
+        <Tabs />
+
+        <div className="cities">
+          <div className="cities__places-container container">
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">{props.numberOfPlaces} places to stay in Amsterdam</b>
+              <form className="places__sorting" action="#" method="get">
+                <span className="places__sorting-caption">Sort by</span>
+                <span className="places__sorting-type" tabIndex={0}>
+                  Popular
+                  <svg className="places__sorting-arrow" width="7" height="4">
+                    <use xlinkHref="#icon-arrow-select"></use>
+                  </svg>
+                </span>
+                <ul className="places__options places__options--custom places__options--opened">
+                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+                  <li className="places__option" tabIndex={0}>Price: low to high</li>
+                  <li className="places__option" tabIndex={0}>Price: high to low</li>
+                  <li className="places__option" tabIndex={0}>Top rated first</li>
+                </ul>
+              </form>
+
+              <div className="cities__places-list places__list tabs__content">
+
+                <p>This is <b>404</b> page, so the requested page not exist, sorry, but please go to <a href="/">main page</a></p>
+
+              </div>
+            </section>
+            <div className="cities__right-section">
+              <section className="cities__map map"></section>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Main;
