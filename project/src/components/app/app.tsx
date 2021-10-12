5d0fc6b70b214b9de2972e37e0c2063fce06db36
Max Vorozhtsov <maxim.vorojtsov@gmail.com>
2021-10-12 21:29:19 +0200

    private routing

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,11 @@
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
-import {AppRoute} from '../../const';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import Error404 from '../error/error';
 import Favorites from '../favorites/favorites';
 import Login from '../login/login';
 import Main from '../main/main';
 import Property from '../property/property';
+import PrivateRoute from '../private-routes/private-routes';
 
 function App(props: {numberOfPlaces: string}): JSX.Element {
   return (
@@ -16,9 +17,13 @@ function App(props: {numberOfPlaces: string}): JSX.Element {
         <Route exact path={AppRoute.SignIn}>
           <Login />
         </Route>
-        <Route exact path={AppRoute.Favorites}>
-          <Favorites />
-        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.Favorites}
+          render={() => <Favorites />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
         <Route exact path={AppRoute.Room}>
           <Property />
         </Route>
--- project/src/const.tsx
+++ project/src/const.tsx
@@ -2,5 +2,11 @@ export enum AppRoute {
   Main = '/',
   SignIn = '/login',
   Favorites = '/favorites',
-  Room = '/offer/:id',
+  Room = '/offer/:id'
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
 }
